337fe1c5b3ddcce1f28a37f1cd3c2162584a4782
Valentyn Lyaskovskiy <riodemouro2017@gmail.com>
2023-02-08 16:51:16 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
@@ -8,3 +10,27 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const tabs = document.querySelectorAll(".best__tab");
+const tabContents = document.querySelectorAll(".best__tab-content-items");
+
+tabs.forEach((item) => {
+  item.addEventListener("click", makeActive);
+});
+
+function makeActive(e) {
+  const clickedTab = e.currentTarget;
+  let relatedContent = clickedTab.dataset.btn;
+
+  tabs.forEach((item) => {
+    item.classList.remove("best__tab_active");
+    clickedTab.classList.add("best__tab_active");
+  });
+
+  tabContents.forEach((item) => {
+    item.classList.remove("best__tab-content-items_active");
+    document
+      .querySelector(`#${relatedContent}`)
+      .classList.add("best__tab-content-items_active");
+  });
+}
